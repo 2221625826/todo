f2d9329900e1290edafc1064998a5aba9d5913f0
zhangyiheng03 <zhangyiheng03@corp.netease.com>
2022-06-01 17:38:04 +0800

    init

---
+++ src/views/TodoView.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="todo">
+    <MyTodo></MyTodo>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import MyTodo from '@/components/MyTodo.vue'
+export default {
+  name: 'TodoView',
+  components: {
+    MyTodo
+  }
+}
+</script>
