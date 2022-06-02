7b3f6574cb6fa584045c1350b47501b288a8b6f8
zhangyiheng03 <zhangyiheng03@corp.netease.com>
2022-06-02 10:03:41 +0800

    封装get post

---
+++ src/axios/index.js
@@ -0,0 +1,27 @@
+import axios from 'axios';
+axios.defaults.baseURL = '/api' //此路径为配置代理服务器时的代理路径
+
+export default {
+    get(url, data) {
+        return new Promise((resolve, reject) => {
+            axios.get(url, {
+                params: data
+            }).then(response => {
+                resolve(response.data)
+            }).catch(err => {
+                reject(err)
+            })
+        })
+    },
+    post(url, data) {
+        return new Promise((resolve, reject) => {
+            axios.post(url,
+                data
+            ).then(response => {
+                resolve(response.data);
+            }).catch(err => {
+                reject(err);
+            })
+        })
+    }
+};
--- src/components/MyTodo.vue
+++ src/components/MyTodo.vue
@@ -1,21 +1,37 @@
 <template>
-    <div>
-        <ul>
-          <li>test</li>
-        </ul>
-    </div>
+  <div>
+    <ul>
+      <li>{{ myMsg }}</li>
+    </ul>
+    <button @click="remote">按钮</button>
+  </div>
 </template>
 
 <script>
 export default {
-  name: 'MyTodo',
+  name: "MyTodo",
   props: {
-    msg: String
-  }
-}
+    msg: String,
+  },
+  data() {
+    return {
+      myMsg: "local",
+    };
+  },
+  methods: {
+    remote: function () {
+      this.$axios
+        .get("/school/remote")
+        .then((res) => {
+          if (res.code == 200) {
+            this.myMsg = res.result;
+          }
+        });
+    },
+  },
+};
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-
 </style>
